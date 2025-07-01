3a524fa3d90800a2a3d2c08b367288662f685c19
Gnanika Omkarini Makkena <gnanikaomkarini@gmail.com>
2025-07-02 00:22:12 +0530

    Cookie stored after registering

--- Haskell/register.hs
+++ Haskell/register.hs
@@ -8,20 +8,23 @@ import Data.Aeson (encode, object, (.=))
 import Data.Aeson.Lens (key, _String)
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy.Char8 as BL8
+import Data.Default (def)
+import Network.HTTP.Client (CookieJar, destroyCookieJar)
 
-register :: String -> IO String
+register :: String -> IO (String, CookieJar)
 register name = do
     let url = "https://wordle.we4shakthi.in/game/register"
         payload = encode $ object ["mode" .= ("wordle" :: String), "name" .= name]
         opts = defaults
              & header "Content-Type" .~ ["application/json"]
-
     r <- postWith opts url payload
     let playerId = r ^? responseBody . key "id" . _String
-    return $ maybe "No id found" T.unpack playerId
+        cookieJar = r ^. responseCookieJar
+    return (maybe "No id found" T.unpack playerId, cookieJar)
 
 main :: IO ()
 main = do
     let playerName = "gnanika"
-    playerId <- register playerName
+    (playerId, cookieJar) <- register playerName
     putStrLn $ "ID: " ++ playerId
+    putStrLn $ "Session cookies: " ++ show (destroyCookieJar cookieJar)
