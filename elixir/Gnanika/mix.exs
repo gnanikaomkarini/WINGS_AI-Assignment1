b7ce5b4bfe7fb0129546af87cb179043b57ffdfb
Gnanika Omkarini Makkena <gnanikaomkarini@gmail.com>
2025-07-21 19:05:27 +0530

    Elixir: mix file containing dependencies added

---
+++ elixir/Gnanika/mix.exs
@@ -0,0 +1,26 @@
+defmodule Gnanika.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :gnanika,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger, :finch],
+      mod: {Gnanika.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:finch, "~> 0.16"}
+    ]
+  end
+end
